ce1fa78b1f44d60a5b812e21da5e946249ac91fc
mikyungyun <yun.workk@gmail.com>
2023-08-05 05:02:54 +0900

    add dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,5 +7,11 @@ module.exports = {
   theme: {
     extend: {},
   },
+  // 브라우저 설정을 따라감
+  darkMode: "media",
+  // dark: 선택자를 사용하는 요소의 부모요소에
+  // class="dark"를 추가해주어야 다크모드를 따라가게 됨
+  // 그래서 보통은 html의 body태그(가장 상위 요소)에 dark 클래스네임을 추가한다
+  // darkMode: "class",
   plugins: [],
 }
