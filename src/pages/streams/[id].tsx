048ee0952dc079839e8b7a1a7eaaf919042b372e
mikyungyun <yun.workk@gmail.com>
2023-08-12 21:32:03 +0900

    stream detail page ui

---
+++ src/pages/streams/[id].tsx
@@ -0,0 +1,64 @@
+import type { NextPage } from 'next';
+
+const StreamDetail: NextPage = () => {
+  return (
+    <div className="py-10 px-5 space-y-4">
+      <div className="">
+        <div className="w-full aspect-video rounded-md shadow-sm bg-slate-300" />
+        <h3 className="text-2xl font-bold text-gray-700 mt-2">iphone 14 pro</h3>
+      </div>
+      <div className="h-[50vh] py-10 pb-16 space-y-4 overflow-y-scroll">
+        <div className="flex items-start space-x-2">
+          <div className="w-8 h-8 rounded-full bg-slate-300" />
+          <div className="text-sm text-gray-700 w-1/2 p-2 border border-gray-400 rounded-md">
+            <p>Hi how much are you selling them for?</p>
+          </div>
+        </div>
+        <div className="flex items-start flex-row-reverse space-x-2 space-x-reverse">
+          <div className="w-8 h-8 rounded-full bg-slate-300" />
+          <div className="text-sm text-gray-700 w-1/2 p-2 border border-gray-400 rounded-md">
+            <p>I want ￦20,000</p>
+          </div>
+        </div>
+        <div className="flex items-start space-x-2">
+          <div className="w-8 h-8 rounded-full bg-slate-300" />
+          <div className="text-sm text-gray-700 w-1/2 p-2 border border-gray-400 rounded-md">
+            <p>Hi how much are you selling them for?</p>
+          </div>
+        </div>
+        <div className="flex items-start flex-row-reverse space-x-2 space-x-reverse">
+          <div className="w-8 h-8 rounded-full bg-slate-300" />
+          <div className="text-sm text-gray-700 w-1/2 p-2 border border-gray-400 rounded-md">
+            <p>I want ￦20,000</p>
+          </div>
+        </div>
+        <div className="flex items-start space-x-2">
+          <div className="w-8 h-8 rounded-full bg-slate-300" />
+          <div className="text-sm text-gray-700 w-1/2 p-2 border border-gray-400 rounded-md">
+            <p>Hi how much are you selling them for?</p>
+          </div>
+        </div>
+        <div className="flex items-start flex-row-reverse space-x-2 space-x-reverse">
+          <div className="w-8 h-8 rounded-full bg-slate-300" />
+          <div className="text-sm text-gray-700 w-1/2 p-2 border border-gray-400 rounded-md">
+            <p>I want ￦20,000</p>
+          </div>
+        </div>
+        <div className="flex items-start space-x-2">
+          <div className="w-8 h-8 rounded-full bg-slate-300" />
+          <div className="text-sm text-gray-700 w-1/2 p-2 border border-gray-400 rounded-md">
+            <p>Hi how much are you selling them for?</p>
+          </div>
+        </div>
+        <div className="flex items-start flex-row-reverse space-x-2 space-x-reverse">
+          <div className="w-8 h-8 rounded-full bg-slate-300" />
+          <div className="text-sm text-gray-700 w-1/2 p-2 border border-gray-400 rounded-md">
+            <p>I want ￦20,000</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default StreamDetail;
