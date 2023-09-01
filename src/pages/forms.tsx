ac55384f1db3a40c646778a883a6f8c47b65d2f2
mikyungyun <yun.workk@gmail.com>
2023-09-01 20:46:20 +0900

    reactHookForm error handling

--- src/pages/forms.tsx
+++ src/pages/forms.tsx
@@ -4,14 +4,26 @@ interface ILoginForms {
   name: string;
   email: string;
   password: string;
+  errors: string;
 }
 function Forms() {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<ILoginForms>();
-  const onValid = (data: ILoginForms) => {};
+    watch, // 하나의 필드 혹은 모든 필드를 watch할 수 있음
+    setError, // 에러를 의도한 대로 설정할 수 있음
+    setValue,
+    reset, // form을 제출하고 form 전체를 reset하길 원할 때 사용
+    resetField, // 특정한 field를 reset하길 원할 때
+  } = useForm<ILoginForms>({
+    mode: 'onChange',
+  });
+  const onValid = (data: ILoginForms) => {
+    setError('errors', { message: 'error' });
+    // reset();
+    resetField('password');
+  };
   const onInvalid = (errors: FieldErrors) => {
     console.log(errors);
   };
@@ -19,10 +31,17 @@ function Forms() {
   return (
     <form onSubmit={handleSubmit(onValid, onInvalid)}>
       <input
-        {...register('name', { required: 'Name is required' })}
+        {...register('name', {
+          required: 'Name is required',
+          maxLength: {
+            message: 'The name should be less than 10 chars.',
+            value: 10,
+          },
+        })}
         type="text"
         placeholder="Name"
       />
+      {errors.name?.message}
       <input
         {...register('email', {
           required: 'Email is required',
@@ -34,6 +53,7 @@ function Forms() {
         })}
         type="email"
         placeholder="Email"
+        className={`${Boolean(errors.email) ? 'border-2 border-red-500' : ''}`}
       />
       {errors.email?.message}
       <input
@@ -42,6 +62,8 @@ function Forms() {
         placeholder="Password"
       />
       <input type="submit" value="Create Account" />
+      {/* 유저에게 form의 global error를 보여줌 */}
+      {errors.errors?.message}
     </form>
   );
 }
